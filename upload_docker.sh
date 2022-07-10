ac657320fa5ca49134fb7ce542c164a9b8fb59d7
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-07-10 03:56:24 +0100

    updated repository

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=geoffrey13/flaskapp
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u geoffrey13
 
 # Step 3:
 # Push image to a docker repository
+docker tag flaskapp $dockerpath:latest
+docker push $dockerpath:latest
